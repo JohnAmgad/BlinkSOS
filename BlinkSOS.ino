d8425e4af41dffe6f94b89ec052cefdaa93a8099
JohnAmgad <41134098+JohnAmgad@users.noreply.github.com>
2018-07-12 22:22:14 +1000

    Add files via upload

---
+++ BlinkSOS.ino
@@ -0,0 +1,45 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+ digitalWrite(LED_BUILTIN, HIGH);
+ delay(500);
+ digitalWrite(LED_BUILTIN, LOW);
+ delay(1000);
+ digitalWrite(LED_BUILTIN, HIGH);
+ delay(500);
+ digitalWrite(LED_BUILTIN, LOW);
+ delay(1000);
+ digitalWrite(LED_BUILTIN, HIGH);
+ delay(500);
+ digitalWrite(LED_BUILTIN, LOW);
+ delay(1000);
+ digitalWrite(LED_BUILTIN, HIGH);
+ delay(3000);
+ digitalWrite(LED_BUILTIN, LOW);
+ delay(1000);
+ digitalWrite(LED_BUILTIN, HIGH);
+ delay(3000);
+ digitalWrite(LED_BUILTIN, LOW);
+ delay(1000);
+ digitalWrite(LED_BUILTIN, HIGH);
+ delay(3000);
+ digitalWrite(LED_BUILTIN, LOW);
+ delay(1000);
+ digitalWrite(LED_BUILTIN, HIGH);
+ delay(500);
+ digitalWrite(LED_BUILTIN, LOW);
+ delay(1000);
+ digitalWrite(LED_BUILTIN, HIGH);
+ delay(500);
+ digitalWrite(LED_BUILTIN, LOW);
+ delay(1000);
+ digitalWrite(LED_BUILTIN, HIGH);
+ delay(500);
+ digitalWrite(LED_BUILTIN, LOW);
+ delay(1000);
+
+
+}
